19e5b9c133649aee8ed45605814120857a6a96aa
YasirAbbas110 <49227021+YasirAbbas110@users.noreply.github.com>
2019-04-05 04:24:32 -0700

    Add files via upload

---
+++ Lab 1/Task2.java
@@ -0,0 +1,10 @@
+class Task2{
+public static void main(String[] args){
+double d = 12.14;
+int i = (int)d;
+float f = (float)d-i;
+System.out.println(" D = "+d);
+System.out.println(" I = "+i);
+System.out.println(" F = "+f);
+}
+}
---
+++ Lab 1/task1.java
@@ -0,0 +1,18 @@
+class task1{
+public static void main(String[] args){
+String name="Yasir Abbas";
+byte roll=51;
+short marks=77;
+int total=100;
+double percentage = (77/100.0)*100;
+char grade = 'A';
+boolean status=true;
+System.out.println("Name = "+name);
+System.out.println("Roll no = "+roll);
+System.out.println("Obtain marks = "+marks);
+System.out.println("Total marks = "+total);
+System.out.println("Percentage = "+percentage);
+System.out.println("Grade = "+grade);
+System.out.println("Status = "+status);
+}
+}
